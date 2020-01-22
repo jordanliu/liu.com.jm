0eb26c1aa81e0f6fa04eb8f0869104bdd0ebadf4
jordanliu <jordanxavierliu@gmail.com>
2020-01-21 23:40:23 -0500

    404

--- src/components/project.js
+++ src/components/project.js
@@ -1,12 +1,12 @@
 import React from "react"
 import "./layout.css"
 import { RichText } from "prismic-reactjs"
-import { graphql } from "gatsby"
+//import { graphql } from "gatsby"
 //    {console.log(JSON.stringify(projects, null, 4))}
 const project = ({ projects }) => {
   if (!projects) return null
   return (
-    <div class="cards">
+    <div className="cards">
       {projects.map(project => {
         return (
           <div className="card" key={project.node._meta.uid}>
