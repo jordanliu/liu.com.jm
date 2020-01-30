043a46ce4805284b5e6db023c9ca0dc19aac9bbe
jordanliu <jordanxavierliu@gmail.com>
2020-01-30 00:38:35 -0500

    Layout changes - responsive

--- src/components/project.js
+++ src/components/project.js
@@ -6,15 +6,18 @@ import { RichText } from "prismic-reactjs"
 const project = ({ projects }) => {
   if (!projects) return null
   return (
-    <div className="cards">
-      {projects.map(project => {
-        return (
-          <div className="card" key={project.node._meta.uid}>
-            {RichText.asText(project.node.project_title)}
-            {RichText.asText(project.node.project_description)}
-          </div>
-        )
-      })}
+    <div>
+      <h4 className="projects-title">Featured Projects</h4>
+      <div className="cards">
+        {projects.map(project => {
+          return (
+            <div className="card" key={project.node._meta.uid}>
+              {RichText.asText(project.node.project_title)}
+              {RichText.asText(project.node.project_description)}
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
