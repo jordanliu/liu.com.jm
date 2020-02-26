862669c1e23a60b6ad96a79f29e017e3e7311d4b
jordanliu <jordanxavierliu@gmail.com>
2020-02-25 19:38:53 -0500

    css tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Project from "../components/project/project"
 import About from "../components/about/about"
 import Contact from "../components/contact/contact"
 import { graphql } from "gatsby"
-import fadeInUp from "react-animations/lib/fade-in-up"
+import fadeIn from "react-animations/lib/fade-in"
 import Radium, { StyleRoot } from "radium"
 
 export const query = graphql`
@@ -32,11 +32,15 @@ export const query = graphql`
 const styles = {
   fadeInUp: {
     animation: "x 1s",
-    animationName: Radium.keyframes(fadeInUp, "fadeInUp"),
+    animationName: Radium.keyframes(fadeIn, "fadeIn"),
   },
 }
 
 export default ({ data }) => {
+  console.log(
+    "%cHi! Created by Jordan Liu, check out my GitHub: https://github.com/jordanliu",
+    "padding:5px;border-width:thin;border-style: solid;background-color:white;color:black;border-radius:7px;"
+  )
   const projects = data.allMarkdownRemark.edges
   return (
     <StyleRoot style={styles.fadeInUp}>
