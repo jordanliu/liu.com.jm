51c398b1fc5f6f5453dc44afc8b71a166a340fa8
Jordan Liu <jordanxavierliu@gmail.com>
2021-07-02 00:22:07 -0500

    chore: og media update

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,30 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Jordan Liu</title>
-        <meta name="description" content="Jordan Liu - Full Stack Developer based in Kington, Jamaica" />
+        <meta
+          name="description"
+          content="Full Stack Developer based in Kington, Jamaica"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-         Jordan Liu
-        </h1>
+        <h1 className={styles.title}>Jordan Liu</h1>
 
-        <p className={styles.description}>
-        Software Developer
-        </p>
+        <p className={styles.description}>Software Developer</p>
 
-       
-   <div className={styles.cta}><a href="https://github.com/jordanliu">GitHub</a> | <a href="https://twitter.com/jordanxliu">Twitter</a>  | <a href="https://www.linkedin.com/in/jordanxliu/">LinkedIn</a>  | <a href="mailto:jordanxavierliu@gmail.com">Email</a></div>
+        <div className={styles.cta}>
+          <a href="https://github.com/jordanliu">GitHub</a> |{" "}
+          <a href="https://twitter.com/jordanxliu">Twitter</a> |{" "}
+          <a href="https://www.linkedin.com/in/jordanxliu/">LinkedIn</a> |{" "}
+          <a href="mailto:jordanxavierliu@gmail.com">Email</a>
+        </div>
       </main>
     </div>
-  )
+  );
 }
